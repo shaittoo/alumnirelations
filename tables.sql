76af62d317035e1d9e21dbc5a60a529981f1266e
shaittoo <talisayshaina8@gmail.com>
2024-05-29 18:29:11 +0800

    [Log In Register User/Admin]

--- tables.sql
+++ tables.sql
@@ -2,14 +2,13 @@ CREATE TABLE academic_organizations (
     org_id INT AUTO_INCREMENT PRIMARY KEY,
     organization_name VARCHAR(255) NOT NULL UNIQUE
 );
-
 CREATE TABLE degree_programs (
     program_id INT AUTO_INCREMENT PRIMARY KEY,
     degree_program VARCHAR(255) NOT NULL UNIQUE,
     academic_organization_id INT,
-    FOREIGN KEY (academic_organization_id) REFERENCES academic_organizations(org_id) ON DELETE SET NULL
+    FOREIGN KEY (academic_organization_id) REFERENCES academic_organizations(org_id) ON DELETE
+    SET NULL
 );
-
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
@@ -21,13 +20,16 @@ CREATE TABLE users (
     graduating_year YEAR,
     degree_program_id INT,
     academic_organization_id INT,
+    password VARCHAR(255) NOT NULL,
+    cpassword VARCHAR(255) NOT NULL,
+    user_type VARCHAR(255),
     bio TEXT,
     profile_picture VARCHAR(255),
-    FOREIGN KEY (degree_program_id) REFERENCES degree_programs(program_id) ON DELETE SET NULL,
-    FOREIGN KEY (academic_organization_id) REFERENCES academic_organizations(org_id) ON DELETE SET NULL
+    FOREIGN KEY (degree_program_id) REFERENCES degree_programs(program_id) ON DELETE
+    SET NULL,
+        FOREIGN KEY (academic_organization_id) REFERENCES academic_organizations(org_id) ON DELETE
+    SET NULL
 );
-
-
 CREATE TABLE events (
     event_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -35,7 +37,6 @@ CREATE TABLE events (
     event_date DATE,
     image_url VARCHAR(255)
 );
-
 CREATE TABLE event_participants (
     participant_id INT AUTO_INCREMENT PRIMARY KEY,
     event_id INT,
@@ -44,14 +45,11 @@ CREATE TABLE event_participants (
     FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
-
 CREATE TABLE galleries (
     gallery_id INT AUTO_INCREMENT PRIMARY KEY,
     batch_year YEAR NOT NULL,
     gallery_name VARCHAR(255)
 );
-
-
 CREATE TABLE memories (
     memory_id INT AUTO_INCREMENT PRIMARY KEY,
     gallery_id INT,
@@ -60,5 +58,6 @@ CREATE TABLE memories (
     description TEXT,
     uploader_id INT,
     FOREIGN KEY (gallery_id) REFERENCES galleries(gallery_id) ON DELETE CASCADE,
-    FOREIGN KEY (uploader_id) REFERENCES users(user_id) ON DELETE SET NULL
+    FOREIGN KEY (uploader_id) REFERENCES users(user_id) ON DELETE
+    SET NULL
 );
