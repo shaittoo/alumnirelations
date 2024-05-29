785d5ddb6151b5fd452972d491b92f906363284e
shaittoo <talisayshaina8@gmail.com>
2024-05-29 23:46:55 +0800

    changes

--- events_admin.php
+++ events_admin.php
@@ -1,10 +1,9 @@
 <?php
+session_start();
 
 @include 'connect.php';
 
-session_start();
-
-if(!isset($_SESSION['user_name'])){
+if(!isset($_SESSION['admin_name'])){
    header('location:login_form.php');
 }
 
@@ -16,9 +15,7 @@ if(!isset($_SESSION['user_name'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>user page</title>
-
-   <!-- custom css file link  -->
+   <title>admin page</title>
    <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -27,9 +24,9 @@ if(!isset($_SESSION['user_name'])){
 <div class="container">
 
    <div class="content">
-      <h3>hi, <span>user</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
-      <p>this is an user page</p>
+      <h3>hi, <span>admin</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
+      <p>this is an admin page</p>
       <a href="login_form.php" class="btn">login</a>
       <a href="register_form.php" class="btn">register</a>
       <a href="logout.php" class="btn">logout</a>
--- events_user.php
+++ events_user.php
@@ -1,10 +1,9 @@
 <?php
+session_start();
 
 @include 'connect.php';
 
-session_start();
-
-if(!isset($_SESSION['admin_name'])){
+if(!isset($_SESSION['user_name'])){
    header('location:login_form.php');
 }
 
@@ -16,7 +15,7 @@ if(!isset($_SESSION['admin_name'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>admin page</title>
+   <title>user page</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
@@ -27,9 +26,9 @@ if(!isset($_SESSION['admin_name'])){
 <div class="container">
 
    <div class="content">
-      <h3>hi, <span>admin</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
-      <p>this is an admin page</p>
+      <h3>hi, <span>user</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
+      <p>this is an user page</p>
       <a href="login_form.php" class="btn">login</a>
       <a href="register_form.php" class="btn">register</a>
       <a href="logout.php" class="btn">logout</a>
@@ -39,3 +38,4 @@ if(!isset($_SESSION['admin_name'])){
 
 </body>
 </html>
+
